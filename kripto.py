7e3aeb823b74ad7dd57afb2c2e252398eb26e7cd
yudhaprajatma33-ops <yudhaprajatma33@gmail.com>
2026-01-19 17:58:36 +0700

    Update kripto.py

--- kripto.py
+++ kripto.py
@@ -140,10 +140,10 @@ if mode == "Enkripsi":
     col_input1, col_input2 = st.columns(2)
     
     with col_input1:
-        plaintext = st.text_input("Masukkan Plaintext:", "masukkan plaintext", help="Masukkan teks yang akan dienkripsi (huruf A-Z)")
+        plaintext = st.text_input("Masukkan Plaintext:", "", help="Masukkan teks yang akan dienkripsi (huruf A-Z)")
     
     with col_input2:
-        key = st.text_input("Masukkan Key:", "masukkan key", help="Masukkan key (angka, maksimal 5 digit)")
+        key = st.text_input("Masukkan Key:", "", help="Masukkan key (angka, maksimal 5 digit)")
     
     if st.button("🚀 Enkripsi", type="primary"):
         if plaintext and key:
